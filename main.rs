41d21d0a156de86ff24eecb59dc30d2c5e4583e0
ouoz <ouoz@LAPTOP-KGCNIQ4U.localdomain>
2025-10-07 15:37:57 +0900

    initial

---
+++ main.rs
@@ -0,0 +1,12 @@
+use ac_library::*;
+use itertools::*;
+use num::*;
+use proconio::marker::*;
+use proconio::*;
+use std::collections::*;
+use std::mem::*;
+use superslice::*;
+
+fn main() {
+    println!("hello");
+}
